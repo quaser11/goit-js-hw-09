168c2b208fd6334bef91b795d3ec79a241f600aa
Ilya Svintsov <xeri237@gmail.com>
2024-06-07 14:51:35 +0300

    new commit

---
+++ src/js/1-gallery.js
@@ -0,0 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import {galleryItems} from './gallery-items.js';
+
+const imagesList = document.querySelector(".gallery")
+
+function makeImgMarkup(obj) {
+  return obj
+    .map((image) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+           <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="${image.description}"/>
+        </a>
+     </li>`
+    })
+    .join("");
+}
+
+function appendImages(list) {
+  return (list.innerHTML = makeImgMarkup(galleryItems));
+}
+
+
+appendImages(imagesList);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionDelay:250})
---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+
+form.addEventListener('submit', onSubmit)
+form.addEventListener('input', saveData)
+
+setValuesAfterReload()
+
+function onSubmit(e){
+  e.preventDefault()
+
+  console.log(Object.fromEntries(new FormData(form)));
+  email.value = '';
+  message.value = '';
+  localStorage.removeItem('feedback-form-state');
+}
+
+function saveData(e){
+  const data = new FormData(form)
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(Object.fromEntries(data)))
+}
+
+function setValuesAfterReload(){
+  let data = {}
+
+  if(localStorage.getItem('feedback-form-state') !== null) {
+    data = JSON.parse(localStorage.getItem('feedback-form-state'))
+  }
+
+  if(data.hasOwnProperty('email') && data.email !== ''){
+    email.value = data.email
+  }
+
+  if(data.hasOwnProperty('message') && data.message !== ''){
+    message.value = data.message
+  }
+}
